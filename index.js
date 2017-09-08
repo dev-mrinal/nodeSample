634dae43ce2c24ac27cc2ae7d96d609c3794df08
dev-mrinal <mrinal.mondal87@gmail.com>
2017-09-08 18:18:34 +0530

    Create Rest API with NodeJS

---
+++ index.js
@@ -0,0 +1,152 @@
+var http     = require('http'),
+	express  = require('express'),
+	mysql    = require('mysql')
+	parser   = require('body-parser');
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'db_test'
+});
+
+var sessionStore = new MySQLStore({}/* session store options */, connection);
+
+var app = express();
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");    
+} else {
+    console.log("Error connecting database ... nn");    
+}
+});
+
+
+app.get('/', function (req, res) {
+	res.send('<html><body><p>Welcome to sShop 0.1</p></body></html>');
+});
+
+
+
+app.post('/product/create/', function (req,res) {
+	var id = req.params.id, response = [];
+
+	if (
+		typeof req.body.name !== 'undefined' && 
+		typeof req.body.price !== 'undefined' && 
+		typeof req.body.imageUrl !== 'undefined' &&
+		typeof req.body.time !== 'undefined'
+	) {
+		var name = req.body.name, price = req.body.price, imageUrl = req.body.imageUrl, time = req.body.time;
+
+		connection.query('INSERT INTO nd_products values (?,?,?,?,?)', 
+			[id,name, price, imageUrl,time], 
+			function(err, result) {
+		  		if (!err){
+
+					if (result.affectedRows != 0) {
+						response.push({'result' : 'success'});
+					} else {
+						response.push({'msg' : 'No Result Found'});
+					}
+
+					res.setHeader('Content-Type', 'application/json');
+			    	res.status(200).send(JSON.stringify(response));
+		  		} else {
+				    res.status(400).send(err);
+			  	}
+			});
+
+	} else {
+		response.push({'result' : 'error', 'msg' : 'Please fill required details'});
+		res.setHeader('Content-Type', 'application/json');
+    	res.status(200).send(JSON.stringify(response));
+	}
+});
+
+app.put('/product/edit/:id', function (req,res) {
+	var id = req.params.id, response = [];
+
+	if (
+		typeof req.body.name !== 'undefined' && 
+		typeof req.body.price !== 'undefined' && 
+		typeof req.body.imageUrl !== 'undefined'
+	) {
+		var name = req.body.name, price = req.body.price, imageUrl = req.body.imageUrl;
+
+		connection.query('UPDATE nd_products SET product_name = ?, product_price = ?, product_image = ? WHERE id = ?', 
+			[name, price, imageUrl, id], 
+			function(err, result) {
+		  		if (!err){
+
+					if (result.affectedRows != 0) {
+						response.push({'result' : 'success'});
+					} else {
+						response.push({'msg' : 'No Result Found'});
+					}
+
+					res.setHeader('Content-Type', 'application/json');
+			    	res.status(200).send(JSON.stringify(response));
+		  		} else {
+				    res.status(400).send(err);
+			  	}
+			});
+
+	} else {
+		response.push({'result' : 'error', 'msg' : 'Please fill required details'});
+		res.setHeader('Content-Type', 'application/json');
+    	res.status(200).send(JSON.stringify(response));
+	}
+});
+
+
+app.delete('/product/delete/:id', function (req,res) {
+	var id = req.params.id;
+
+	connection.query('DELETE FROM nd_products WHERE id = ?', [id], function(err, result) {
+  		if (!err){
+  			var response = [];
+
+			if (result.affectedRows != 0) {
+				response.push({'result' : 'success'});
+			} else {
+				response.push({'msg' : 'No Result Found'});
+			}
+
+			res.setHeader('Content-Type', 'application/json');
+	    	res.status(200).send(JSON.stringify(response));
+  		} else {
+		    res.status(400).send(err);
+	  	}
+	});
+});
+
+
+app.get('/product/:id', function (req,res) {
+	var id = req.params.id;
+
+	connection.query('SELECT * from nd_products where id = ?', [id], function(err, rows, fields) {
+  		if (!err){
+  			var response = [];
+
+			if (rows.length != 0) {
+				response.push({'result' : 'success', 'data' : rows});
+			} else {
+				response.push({'result' : 'error', 'msg' : 'No Results Found'});
+			}
+
+			res.setHeader('Content-Type', 'application/json');
+	    	res.status(200).send(JSON.stringify(response));
+  		} else {
+		    res.status(400).send(err);
+	  	}
+	});
+});
+
+
+app.listen(3000);
